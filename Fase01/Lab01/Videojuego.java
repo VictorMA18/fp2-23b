4ed6e949dabbf72af270757cc7ba5b888a364b5b
VictorMA18 <vmamanian@unsa.edu.pe>
2023-09-18 16:22:24 -0500

    Creamos el 4to ejercicio donde creamos 5 soldados y imprimimos sus nombres y su vida usando arreglos

--- Fase01/Lab01/Videojuego.java
+++ Fase01/Lab01/Videojuego.java
@@ -1,20 +1,25 @@
-//  Laboratorio Nro 1 - Ejercicio3
+//  Laboratorio Nro 1 - Ejercicio4
 //  Autor: Mamani Anahua Victor Narciso
 //  Colaboro:
 //  Tiempo: 
 import java.util.*;
-	public class Videojuego {
-		public static void main(String args[]){
-			//1. DECLARAMOS NUESTRO ARREGLO Y TAMBIEN LO INSTANCIAMOS
-			Scanner sc = new Scanner(System.in);
-			String[] names = new String[5];
-			//2. LLENAMOS CADA ELEMENTO DEL ARREGLO
-			for(int i = 0; i < 5; i++){
-				names[i] = sc.nextLine();
-			}	
-			//3. MOSTRAMOS EL CONTENIDO DE CADA ELEMENTO DEL ARREGLO
-			for(int x = 0; x < 5 ; x++){
-				System.out.println("El nombre del soldado numero " + (x + 1) + " es: " + names[x]);
-			}
-		}
-	}
+class Videojuego {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        //DECLARAMOS NUESTROS ARREGLOS
+        String[] name = new String[5];
+        int[] health = new int[5];
+        //CREAMOS UN CICLO PARA LA INFORMACION DE LOS 5 SOLDADOS
+        for(int i = 0; i < 5; i++){
+            System.out.println("Ingresen sus nombres: ");
+            name[i] = sc.next();
+            System.out.println("Ingresen sus vidas:");
+            health[i] = sc.nextInt();
+        }
+        //CREAMOS UN CICLO PARA DESPUES MOSTRARLOS
+        for(int x = 0; x < 5; x++){
+            System.out.println("El nombre del soldado " + (x + 1) + " es: " + name[x]);
+            System.out.println("La vida del soldado " + (x + 1) + " es: " + health[x]);
+        }
+    }
+}
