3ba46373417f6062ec540745635a37902e53c786
VictorMA18 <vmamanian@unsa.edu.pe>
2023-09-18 16:24:58 -0500

    En el 5toejercicio creamos 2 ejercitos y definimos un ganador o un empate dependiendo de su numero de soldados de cada ejercito

--- Fase01/Lab01/Videojuego.java
+++ Fase01/Lab01/Videojuego.java
@@ -1,25 +1,40 @@
-//  Laboratorio Nro 1 - Ejercicio4
+//  Laboratorio Nro 1 - Ejercicio5
 //  Autor: Mamani Anahua Victor Narciso
 //  Colaboro:
-//  Tiempo: 
+//  Tiempo:   
 import java.util.*;
-class Videojuego {
+public class Videojuego {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        //DECLARAMOS NUESTROS ARREGLOS
-        String[] name = new String[5];
-        int[] health = new int[5];
-        //CREAMOS UN CICLO PARA LA INFORMACION DE LOS 5 SOLDADOS
-        for(int i = 0; i < 5; i++){
-            System.out.println("Ingresen sus nombres: ");
-            name[i] = sc.next();
-            System.out.println("Ingresen sus vidas:");
-            health[i] = sc.nextInt();
+        //DECLARAMOS Y INSTACIAMOS NUESTROS ARREGLOS
+        Random random = new Random();
+        String[] Army1 = new String[random.nextInt(5) + 1];
+        String[] Army2 = new String[random.nextInt(5) + 1]; 
+        //LLENAMOS CADA ARREGLO CON LO PEDIDO
+        for(int x = 0; x < Army1.length; x++){
+            Army1[x] = "Soldado" + x;
         }
-        //CREAMOS UN CICLO PARA DESPUES MOSTRARLOS
-        for(int x = 0; x < 5; x++){
-            System.out.println("El nombre del soldado " + (x + 1) + " es: " + name[x]);
-            System.out.println("La vida del soldado " + (x + 1) + " es: " + health[x]);
+        for(int y = 0; y < Army2.length; y++){
+            Army2[y] = "Soldado" + y;
+        }
+        //MOSTRAMOS LOS DATOS DE LOS SOLDADOS DE AMBOS EJERCITOS
+        System.out.println("LOS SOLDADOS DEL EJERCITO 1 SON: ");
+        for(int z = 0; z < Army1.length; z++){
+            System.out.println(Army1[z]);
+        }
+        System.out.println("LOS SOLDADOS DEL EJERCITO 2 SON: ");
+        for(int i = 0; i < Army2.length; i++){
+            System.out.println(Army2[i]);
+        }
+        //MOSTRAMOS EL GANADOR DE LA BATALLA
+        System.out.println("EL RESULTADO DE LA BATALLA ES :");
+        if(Army1.length > Army2.length){
+            System.out.println("EL EJERCITO 1 ES EL GANADOR");
+        }else{
+            if(Army1.length < Army2.length){
+                System.out.println("EL EJERCITO 2 ES EL GANADOR");
+            }else{
+                System.out.println("EMPATE");
+            }
         }
     }
 }
