1c5c3e4143d48674583ad88acf2a269687b71597
VictorMA18 <vmamanian@unsa.edu.pe>
2023-09-18 16:14:13 -0500

    Creamos el 2do ejericio donde creamos 5 soldados y imprimimos sus nombres y su vida y tambien borramos el archivo Sumadig.java ya que no lo necesitamos

--- Fase01/Lab01/Videojuego.java
+++ Fase01/Lab01/Videojuego.java
@@ -1,18 +1,24 @@
-//  Laboratorio Nro 1 - Ejercicio1
+//  Laboratorio Nro 1 - Ejercicio2
 //  Autor: Mamani Anahua Victor Narciso
 //  Colaboro:
 //  Tiempo: 
 import java.util.*;
-	public class Ejercicio1{
+	public class Videojuego{
 		public static void main(String args[]){
 			//1. DECLARAMOS LAS VARIABLES
 			Scanner sc = new Scanner(System.in);
 			String name;
-			//2. LE PEDIMOS A LOS 5 USUARIOS ESCRIBIR SUS NOMBRES
+			int health;
+			//2. LE PEDIMOS A LOS 5 USUARIOS ESCRIBIR SUS NOMBRES Y SU NIVEL DE VIDA	
 			for(int i = 0; i < 5; i++){
 				System.out.println("Ingrese su nombre: ");
 				name = sc.nextLine();
 				System.out.println("El nombre del soldado " + (i + 1) + " es: " + name);
+				for(int x = 0; x < 1; x++){
+					System.out.println("Ingrese su nivel de vida: ");
+					health = sc.nextInt();
+					System.out.println("El nivel de vida del soldado " + (x + 1) + " es: " + health);
+				}
 			}
 		}
 	}
