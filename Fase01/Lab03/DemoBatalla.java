88c9d238be42f5b4847c84ff981d6c54bdef2d7a
VictorMA18 <vmamanian@unsa.edu.pe>
2023-09-24 19:44:35 -0500

    Completamos el metodo mostrarNaves y en esta usamos un for each y el metodo toString() para mostrar datos de cada objeto y tambien le ponemos un tamaño al arreglo de 2 hacer pruebas

---
+++ Fase01/Lab03/DemoBatalla.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class DemoBatalla {
+    public static void main(String [] args){
+        Nave [] misNaves = new Nave[2]; // LE PONEMOS AL ARREGLO UN TAMAÑO DE 2 PARA SU POSTERIOR PRUEBA 
+        Scanner sc = new Scanner(System.in);
+        String nomb, col;
+        int fil, punt;
+        boolean est;
+    for (int i = 0; i < misNaves.length; i++) {
+        System.out.println("Nave " + (i+1) + " : ");
+        System.out.print("Nombre: ");
+        nomb = sc.next();
+        System.out.print("Fila: ");
+        fil = sc.nextInt();
+        System.out.print("Columna: ");
+        col = sc.next();
+        System.out.print("Estado: ");
+        est = sc.nextBoolean();
+        System.out.print("Puntos: ");
+        punt = sc.nextInt();
+        misNaves[i] = new Nave(); //Se crea un objeto Nave y se asigna su referencia a misNaves
+        misNaves[i].setNombre(nomb);
+        misNaves[i].setFila(fil);
+        misNaves[i].setColumna(col);
+        misNaves[i].setEstado(est);
+        misNaves[i].setPuntos(punt);
+    }
+        System.out.println("\nNaves creadas:");
+        System.out.println("--------------------------------------");
+        mostrarNaves(misNaves);
+        System.out.println("--------------------------------------");
+        mostrarPorNombre(misNaves);
+        System.out.println("--------------------------------------");        
+        mostrarPorPuntos(misNaves);
+        System.out.println("--------------------------------------");
+        System.out.println("\nNave con mayor número de puntos: " + mostrarMayorPuntos(misNaves));
+        System.out.println("--------------------------------------");
+    }
+    //Método para mostrar todas las naves
+    public static void mostrarNaves(Nave [] flota){ //COMPLETAMOS EL METODO mostrarNaves Y NOS AYUDAMOS DE UN FOR EACH PARA EL MUESTREO DE LOS DATOS DE CADA OBJETO Y TAMBIEN USAMOS EL METODO toString()
+        for(Nave ship: flota){
+            System.out.println(ship.toString());
+            System.out.println("***********************");
+        }
+    }
+    //Método para mostrar todas las naves de un nombre que se pide por teclado
+    public static void mostrarPorNombre(Nave [] flota){
+    }
+    //Método para mostrar todas las naves con un número de puntos inferior o igual
+    //al número de puntos que se pide por teclado
+    public static void mostrarPorPuntos(Nave [] flota){
+    }
+    //Método que devuelve la Nave con mayor número de Puntos
+    public static Nave mostrarMayorPuntos(Nave [] flota){
+        return null;
+    }
+    //Crear un método que devuelva un nuevo arreglo de objetos con todos los objetos previamente ingresados
+    //pero aleatoriamente desordenados
+}
