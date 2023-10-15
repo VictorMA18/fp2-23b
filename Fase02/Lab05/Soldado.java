8ab8cf21f602fa1870680bd9440f368e80877e06
VictorMA18 <vmamanian@unsa.edu.pe>
2023-10-14 19:01:41 -0500

    Agregando la clase Soldado la cual reutilizamos donde agregamos un metodo constructor y tambien añadimos los atributos row y column con sus respectivos getters y setters

---
+++ Fase02/Lab05/Soldado.java
@@ -0,0 +1,54 @@
+// Laboratorio Nro 5  - Ejercicio Soldado
+// Autor: Mamani Anahua Victor Narciso
+// Colaboro:
+// Tiempo:
+public class Soldado { //CREAMOS LA CLASE SOLDODADO PARA PODER USAR UN ARREGLO BIDIMENSIONAL DONDE NECESITAMOS LA VIDA , EL NOMBRE DEL SOLDADO Y TAMBIEN SU POSICION COMO LA FILA Y LA COLUMNA   
+
+    private String name;
+    private int heatlh; 
+    private int row;
+    private String column;
+
+    //Constructor
+    public Soldado(String name, int health, int row, String column){
+      this.name = name;
+      this.health = health;
+      this.row = row;
+      this.column = column;
+    }
+
+    // Metodos mutadores
+    public void setName(String n){
+        name = n;
+    }
+    public void setHealth(int p){
+        heatlh = p;
+    }
+    public void setRow(int b){
+        row = b;
+    }
+    public void setColumn(String c){
+        column = c; 
+    }
+
+    // Metodos accesores
+    public String getName(){
+        return name;
+    }
+    public int getHealth(){
+        return heatlh;
+
+    }
+    public int getRow(){
+        return row;
+    }
+    public String getColumn(){
+        return column;
+    }
+
+    // Completar con otros métodos necesarios
+    public String toString(){ //CREAMOS ESTE METODO PARA IMPRIMIR LOS DATOS DEl OBJETO
+        String join = "\nNombre: " + getName() + "\nVida: " + getHealth() + "\nFila: " + getRow() + "\nColumna: " + getColumn(); //Agregamos un espaciador para poder separar
+        return join;
+    }
+}
