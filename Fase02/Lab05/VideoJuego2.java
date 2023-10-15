b45908df2bfd22af8e05907c5dfdc6cd504f4d6a
VictorMA18 <vmamanian@unsa.edu.pe>
2023-10-15 00:02:11 -0500

    Completando el metodo fillarray() para que este pueda imprmir los datos de los soldados que estan en el tablero este te devuelve el array lleno con la cantidad de soldados y texto que dice su informacion de cada soldado Y tambien cumplimos con el cual no se repita un soldado en el mismo casillero ya que al ver que este lleno este retrocedera una repeticion ya que estaria tomando la repeticion en un casillero lleno

--- Fase02/Lab05/VideoJuego2.java
+++ Fase02/Lab05/VideoJuego2.java
@@ -10,7 +10,7 @@ class VideoJuego2 {
        for(int i = 0; i < army.length; i++ ){
               for(int j = 0; j < army[i].length; j++){
                      if(army[i][j] != null){
-                            System.out.print("|  " + "X" + "  ");
+                            System.out.print("|   " + "X" + "   ");//ARREGLANDO PARA QUE SE PUEDA IMPRIMIR DE BUENA FORMA
                      }else{
                             System.out.print("|\t");
                      }
@@ -22,22 +22,28 @@ class VideoJuego2 {
   public static Soldado[][] fillarray(int number){
        Random rdm = new Random();
        Soldado[][] army= new Soldado[10][10];
+       System.out.println("Registrando soldados .......");
        for(int i = 0; i < number; i++){
               String name = "Soldado" + (i + 1);
               int health = rdm.nextInt(5) + 1;
               int row = rdm.nextInt(10) + 1;
               String column = String.valueOf((char)(rdm.nextInt(10) + 65));  
-              System.out.println(column);    
+              if(army[row - 1][(int)column.charAt(0) - 65] == null){
+                     System.out.print("*********************************");
+                     army[row - 1][(int)column.charAt(0) - 65] = new Soldado(name, health, row, column);
+                     System.out.println(army[row - 1][(int)column.charAt(0) - 65].toString());
+              }else{
+                     i -= 1;
+              }
        }
-       return null;
+       return army;
   }
   public static void main (String args[]){
        Random rdm = new Random();
        System.out.println("Cuantos soldados? ");
        int numsoldiers = rdm.nextInt(10) + 1;
        System.out.println(numsoldiers);
-       Soldado[][] army = new Soldado[10][10];
+       Soldado[][] army = fillarray(numsoldiers);
        viewboard(army);
-       fillarray(numsoldiers);
   }
 }
