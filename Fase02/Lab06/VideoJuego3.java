6c975c39abe99fcc978161a97401f89326342633
VictorMA18 <vmamanian@unsa.edu.pe>
2023-10-16 01:39:10 -0500

    Creamos el metodo longerLife() el cual nos permitira conocer del soldado de cada ejercito con mayor puntos de vida para esto hacemos una comprobacion con cada uno de estos para poder despues compararlos eny despues de tener el soldado con mayor puntos de vida se imprimira sus datos para ver de quien se trata y a que ejercito

--- Fase02/Lab06/VideoJuego3.java
+++ Fase02/Lab06/VideoJuego3.java
@@ -55,9 +55,28 @@ class VideoJuego3{
         }
         System.out.println("\n*********************************");
     }
+    public static void longerLife(ArrayList<ArrayList<Soldado>> army, int num){
+        System.out.println("El soldado con mayor vida del Ejercito " + num + " es: "); //METODO CREADO PARA PODER PERMITIRNOS A CONOCER EL SOLDADO CON MAYOR VIDA DE CADA EJERCITO 
+        int mayor = 0;
+        Soldado soldier = null;
+        for(int i = 0; i < army.size(); i++){
+            for(int j = 0; j < army.get(i).size(); j++){
+                if(army.get(i).get(j) != null){ //COMPROBACION QUE HACEMOS PARA PODER DECIR QUE EL CASILLERO DONDE ESTAMOS ES UN SOLDADO QUE EXISTE
+                    if(army.get(i).get(j).getHealth() > mayor){ //COMPARAMOS PUNTOS DE VIDA DE CADA SOLDADO PARA VER QUIEN ES EL MAYOR 
+                        mayor = army.get(i).get(j).getHealth();
+                        soldier = army.get(i).get(j);
+                    }
+                }
+            }
+        }
+        System.out.println(soldier.toString());//IMPRIMIMOS SUS DATOS PARA PODER VER DE QUE SOLDADO SE TRATA 
+        System.out.println("*********************************");
+    }
     public static void main(String args[]){
         ArrayList<ArrayList<Soldado>> army1 = fillregisterSoldiers(1);
         ArrayList<ArrayList<Soldado>> army2 = fillregisterSoldiers(2);
         viewBoard(army1, army2);
+        longerLife(army1, 1);
+        longerLife(army2, 2);
     }
 }
