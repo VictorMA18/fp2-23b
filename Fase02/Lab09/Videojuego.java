f37aecbd44129934e7a241071aac26a1d04046f5
VictorMA18 <vmamanian@unsa.edu.pe>
2023-10-29 19:10:19 -0500

    Creamos el metodo viewBoard() el cual nos va a poder imprimir el tablero en donde se va a poder reconocer los soldados de cada ejercito para esto tendriamos que hacer una comparacion de que soldado esta mas fuerte por reclamar su casillero el cual si uno de estos es mas fuerto este se va poder quedar con el casillero en caso de que los 2 sean igual de fuertes en vida entonces los 2 moririan para esto se tuvo que hacer ciertas condiciones al final de esto nos dara la tabla la cual esta va a ser graficada

--- Fase02/Lab09/Videojuego.java
+++ Fase02/Lab09/Videojuego.java
@@ -1,3 +1,7 @@
+// Laboratorio Nro 9  - Ejercicio Videojuego
+// Autor: Mamani Anahua Victor Narciso
+// Colaboro:
+// Tiempo:
 import java.util.*;
 class Videojuego { 
     public static ArrayList<ArrayList<Soldado>> fillRegister(int num){
@@ -21,7 +25,7 @@ class Videojuego {
             String column = String.valueOf((char)(rdm.nextInt(10) + 65)); //REUTILIZAMOS CODIGO DEL ANTERIOR ARCHIVO VIDEOJUEGO2.JAVA YA QUE TENDRIAN LA MISMA FUNCIONALIDAD
             //System.out.println(army.get(row - 1).get((int)column.charAt(0) - 65)); PRUEBA QUE SE HIZO PARA COMPROBAR SI EL OBJETO SE ESTABA DANDO O NO CAPAZ NI EXISTIA  
             if(army.get(row - 1).get((int)column.charAt(0) - 65) == null){
-                System.out.print("Registrando al " + (i + 1) + " soldado del Ejercito " + num + "");
+                System.out.println("Registrando al " + (i + 1) + " soldado del Ejercito " + num + "");
                 army.get(row - 1).set((int)column.charAt(0) - 65, new Soldado(name, health, row, column));
                 army.get(row - 1).get((int)column.charAt(0) - 65).setSpeed(speed);
                 System.out.println(army.get(row - 1).get((int)column.charAt(0) - 65).toString());
@@ -33,8 +37,45 @@ class Videojuego {
         System.out.println("*********************************");
         return army;
     }
+    public static void viewBoard(ArrayList<ArrayList<Soldado>> army1, ArrayList<ArrayList<Soldado>> army2){ //EN ESTE METODO DEMOSTRAREMOS LA TABLA REUTILIZAREMOS CODIGOS DE ANTERIORES LABORATORIOS PARA PODER HACER LA BASE DE ESTE TABLERO
+        System.out.println("\nMostrando tabla de posicion ... --");
+        System.out.println("Leyenda: Ejercito1 --> X | Ejercito2 --> Y"); //RECONOCIMIENTO PARA LOS EJERCITOS Y POSICION DE SUS SOLDADOS
+        System.out.println("\n \t   A\t   B\t   C\t   D\t   E\t   F\t   G\t   H\t   I\t   J"); // RECONOCIMIENTO PARA CADA UBICACION DE CADA SOLDADO EN EL TABLERO POR PARTE DE LAS COLUMNAS
+        System.out.println("\t_________________________________________________________________________________");
+        for(int i = 0; i < 10; i++ ){
+            System.out.print((i + 1) + "\t"); // RECONOCIMIENTO PARA CADA UBICACION DE CADA SOLDADO EN EL TABLERO POR PARTE DE LAS FILAS
+                for(int j = 0; j < 10; j++){
+                        if(army1.get(i).get(j) != null && army2.get(i).get(j) != null){ //CREAMOS UN IF PARA QUE ESTE NOS AYUDE A SABER QUIEN DE ESTOS SOLDADOS SE OCUPARA DEL CASILLERO EL CUAL DONDE ESTAN PELEANDO
+                            if(army1.get(i).get(j).getLifeActual() > army2.get(i).get(j).getLifeActual()){
+                                army1.get(i).get(j).setLifeActual(army1.get(i).get(j).getLifeActual() - army2.get(i).get(j).getLifeActual()); //Cambiamos 
+                                army2.get(i).set(j, null); 
+                                System.out.print("|   " + "X" + "   ");
+                            }else if(army2.get(i).get(j).getLifeActual() > army1.get(i).get(j).getLifeActual()){
+                                army2.get(i).get(j).setLifeActual(army2.get(i).get(j).getLifeActual() - army1.get(i).get(j).getLifeActual());
+                                army1.get(i).set(j, null);;
+                                System.out.print("|   " + "Y" + "   ");
+                            }else{
+                                army2.get(i).set(j, null);
+                                army1.get(i).set(j, null);
+                                System.out.print("|   " + " " + "   ");
+                            }
+                        }else if(army1.get(i).get(j) != null){
+                            System.out.print("|   " + "X" + "   ");
+                        }else if(army2.get(i).get(j) != null){
+                            System.out.print("|   " + "Y" + "   ");
+                        }else{
+                            System.out.print("|   " + " " + "   ");
+                        }
+                }
+                System.out.println("|");
+                System.out.println("\t|_______|_______|_______|_______|_______|_______|_______|_______|_______|_______|");
+        }
+        System.out.println("\n*********************************");
+    
+    }
     public static void main(String args[]){
         ArrayList<ArrayList<Soldado>> army1 = fillRegister(1);
         ArrayList<ArrayList<Soldado>> army2 = fillRegister(2);
+        viewBoard(army1, army2);
     }
 }
