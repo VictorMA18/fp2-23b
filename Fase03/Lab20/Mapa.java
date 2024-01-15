ccdcc5edc6b4013b270721987807c7d96dab5c02
VictorMA18 <vmamanian@unsa.edu.pe>
2024-01-15 13:10:34 -0500

    Agregando las funciones averageLife() y rankingInsercionLife() las cuales la primera funcion me devolvera el promedio de vida del primer ejercito y el segundo metodo me dara el ranking respecto a su nivel de Vida de cada soldado del mayor a menor por el metodo de ordenamiento que es insercion

--- Fase03/Lab20/Mapa.java
+++ Fase03/Lab20/Mapa.java
@@ -33,6 +33,10 @@ public class Mapa {
                 viewBoard(army1, army2);
                 longerLife(army1, kingdom1);
                 longerLife(army2, kingdom2);
+                double avg1 = averageLife(army1, kingdom1);
+                double avg2 = averageLife(army2, kingdom2);
+                rankingInsercionLife(army1, kingdom1);
+                rankingInsercionLife(army2, kingdom2);
             }else{
                 if(resbattle == 2){
                     break;
@@ -280,6 +284,73 @@ public class Mapa {
         }
         System.out.println("El soldado con mayor vida del Ejercito " + kingdom + " es: ");
         System.out.println(soldier.toString());//IMPRIMIMOS SUS DATOS PARA PODER VER DE QUE SOLDADO SE TRATA 
+        System.out.println("*********************************");
+    }
+    public static double averageLife(ArrayList<ArrayList<Soldado>> army , String kingdom){
+        int sum = 0;
+        int count = 0;
+        System.out.println("El promedio de puntos de vida del Ejercito " + kingdom + " es: ");
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){ //ITERACION LA CUAL NOS AYUDA A PASAR POR TODOS LOS SOLDADOS DE CADA EJERCITO
+                if(army.get(i).get(j) != null){ 
+                    sum += army.get(i).get(j).getLifeActual();
+                    count++;
+                }
+            }
+        }
+        if(sum != 0){
+            double avg = sum / (count * 1.0);
+            System.out.println(avg); // DAMOS A CONOCER EL PROMEDIO DE VIDA DE CADA EJERCITO 
+            System.out.println("*********************************");
+            return avg;
+        }else{
+            double avg = 0;
+            System.out.println(avg); // DAMOS A CONOCER EL PROMEDIO DE VIDA DE CADA EJERCITO 
+            System.out.println("*********************************");
+            return avg;
+        }
+    }
+    public static void rankingInsercionLife(ArrayList<ArrayList<Soldado>> army , String kingdom){
+        System.out.println("\nEl Ejercito " + kingdom + " ordenando por metodo insercion: ");
+        int count = 0;
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){ //ITERACION LA CUAL NOS AYUDA A PASAR POR TODOS LOS SOLDADOS DE CADA EJERCITO
+                if(army.get(i).get(j) != null){ 
+                   count++;
+                }
+            }
+        }
+        System.out.println("------------------------------------------");
+        System.out.println("Mostrando Ranking del Ejercito " + kingdom + " ..... ////// --->");
+        Soldado[] soldados = new Soldado[count];
+        int x = 0;
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){ //ITERACION LA CUAL NOS AYUDA A PASAR POR TODOS LOS SOLDADOS AL ARRAY SOLDADO PARA PODER USAR EL USO DEL METODO DE ORDENACION INSERCION
+                if(army.get(i).get(j) != null){ 
+                    if(count - count + x == count){
+                        break;
+                    }else{
+                        soldados[count - count + x] = army.get(i).get(j); //LA MISMA LOGICA QUE EL ANTERIOR METODO SOLO QUE EN ESTE LO USARIAMOS DE MANERA DIFERENTE YA QUE ESTE SERIA DE FORMA DE INSERCION
+                    }
+                    x++;   
+                }
+            }
+        }
+        int n = soldados.length;
+        for (int i = 1; i < n; i++) {
+            Soldado actual = soldados[i];
+            int j = i - 1;
+            while (j >= 0 && soldados[j].getLifeActual() < actual.getLifeActual()) { //ORDENAMOS EL EJERCITO RESPECTIVAMENTE MEDIANTE EL METODO QUE NOS OFRECE INSERCION EL CUAL ES ESTE CODIGO
+                soldados[j + 1] = soldados[j];
+                j--;
+            }
+            soldados[j + 1] = actual;
+        }
+        for(int i = 0; i < soldados.length; i++){
+            System.out.print("\n" + "Puesto " + (i + 1));
+            System.out.println(soldados[i].toString()); //PUBLICAMOS RESULTADOS
+            System.out.println("------------------");
+        }
         System.out.println("*********************************");
     }
 }
